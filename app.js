68e3ab4418bc205a1e567dfef463245cb0068822
Unn4m3DD <guerreiroapatacas@gmail.com>
2020-02-27 16:47:11 +0000

    finalização função de setup do render

---
+++ app.js
@@ -0,0 +1,4 @@
+
+  import renderer from "./renderer.js";
+  const {generate_table} = renderer
+  generate_table("board", 8, 8);
--- renderer.js
+++ renderer.js
@@ -1,16 +1,16 @@
 export default {
-  generate_table(target_id) {
+  generate_table: (target_id, height, width) => {
     let table_body = document.createElement("tbody")
-    for (let x = 0; x < board.length; x++) {
-      let table_row = document.createElement("trow")
-      for (let y = 0; y < board[x].length; y++) {
+    for (let x = 0; x < height; x++) {
+      let table_row = document.createElement("tr")
+      for (let y = 0; y < width; y++) {
         table_row.appendChild(document.createElement("td"))
       }
       table_body.appendChild(table_row)
     }
     document.getElementById(target_id).appendChild(table_body)
   },
-  render(board, table) {
+  render: (board, table) => {
     for (let x = 0; x < board.length; x++) {
       for (let y = 0; y < board[x].length; y++) {
 
