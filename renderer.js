9d99bd76cf7d208cb1a68b3bd370da4fb5abb76d
Unn4m3DD <guerreiroapatacas@gmail.com>
2020-02-27 16:26:55 +0000

    criação de um exemplo simples, implementação da funcao de criação do tabuleiro

---
+++ renderer.js
@@ -0,0 +1,19 @@
+export default {
+  generate_table(target_id) {
+    let table_body = document.createElement("tbody")
+    for (let x = 0; x < board.length; x++) {
+      let table_row = document.createElement("trow")
+      for (let y = 0; y < board[x].length; y++) {
+        table_row.appendChild(document.createElement("td"))
+      }
+      table_body.appendChild(table_row)
+    }
+  },
+  render(board, table) {
+    for (let x = 0; x < board.length; x++) {
+      for (let y = 0; y < board[x].length; y++) {
+
+      }
+    }
+  }
+}
