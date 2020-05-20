9782697547cf30fe318bcbfa11d5610f73c633d9
Unn4m3DD <guerreiroapatacas@gmail.com>
2020-05-20 18:44:49 +0100

    updates compilam e copiam as files para a pasta standalone compiler

---
+++ update.bat
@@ -0,0 +1,4 @@
+IF NOT "%1"=="" ant -f .\ABDLCompilerSource\abdlcompilersource.xml
+IF NOT "%1"=="" ant -f .\BDLCompilerSource\bdlcompilersource.xml
+copy .\ABDLCompilerSource\out\artifacts\ABDLCompilerSource_jar\ABDLCompilerSource.jar .\StandaloneCompiler\ABDLCompiler.jar
+copy .\BDLCompilerSource\out\artifacts\BDLCompiler_jar\BDLCompiler.jar .\StandaloneCompiler\BDLCompiler.jar
--- update.sh
+++ update.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+if [ -z $0 ] 
+then
+ant -f ./ABDLCompilerSource/abdlcompilersource.xml
+ant -f ./BDLCompilerSource/bdlcompilersource.xml
+fi
+cp ./BDLCompilerSource/out/artifacts/BDLCompiler_jar/BDLCompiler.jar ./StandaloneCompiler/BDLCompiler.jar
+echo "BDL copiado"
 cp ./ABDLCompilerSource/out/artifacts/ABDLCompilerSource_jar/ABDLCompilerSource.jar ./StandaloneCompiler/ABDLCompiler.jar
-cp ./BDLCompilerSource/out/artifacts/Compiler_jar/Compiler.jar ./StandaloneCompiler/BDLCompiler.jar
+echo "ABDL copiado"
