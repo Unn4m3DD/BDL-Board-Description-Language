3fec0de0a8b4169f11416644fce17ea8a087b7c4
Unn4m3DD <guerreiroapatacas@gmail.com>
2020-05-21 19:04:43 +0100

    condiçao de fim das damas chinesas

--- StandaloneCompiler/damas/finishing.js
+++ StandaloneCompiler/damas/finishing.js
@@ -1,31 +1,34 @@
 import resolve_moves from "./resolve_moves.js"
 import pieces from "./pieces.js"
 export default [
-  (context) => {
-    for (let x = 0; x < context.width; x++) {
-      for (let y = 0; y < context.width; y++) {
-        if (context.board[x][y].piece
-          && context.board[x][y].piece.owner === context.current_player) {
-          let temp_board = JSON.parse(JSON.stringify(context.board))
-          resolve_moves(
-            pieces[temp_board[x][y].piece.name].moves,
-            x,
-            y,
-            {
-              board: temp_board,
-              height: context.height,
-              width: context.width,
-              current_player: context.current_player,
-              focused: context.focused
-            },
-            true)()
-          const targets = temp_board.map((e) => e.map((e2) => e2.target))
-          for (let i = 0; i < context.width; i++)
-            for (let j = 0; j < context.width; j++)
-              if (targets[i][j]) return false
-        }
-      }
+
+    (context) => {
+      try {
+        if (
+          context.board[0][6].piece.name == "D2" &&
+          context.board[1][6].piece.name == "D2" &&
+          context.board[2][6].piece.name == "D2" &&
+          context.board[3][6].piece.name == "D2" &&
+          context.board[0][5].piece.name == "D2" &&
+          context.board[1][5].piece.name == "D2" &&
+          context.board[2][5].piece.name == "D2" &&
+          context.board[0][4].piece.name == "D2" &&
+          context.board[1][4].piece.name == "D2" &&
+          context.board[0][3].piece.name == "D2") return true;
+      } catch (e) { }
+      try {
+        if (
+          context.board[6][0].piece.name == "D1" &&
+          context.board[6][1].piece.name == "D1" &&
+          context.board[6][2].piece.name == "D1" &&
+          context.board[6][3].piece.name == "D1" &&
+          context.board[5][0].piece.name == "D1" &&
+          context.board[5][1].piece.name == "D1" &&
+          context.board[5][2].piece.name == "D1" &&
+          context.board[4][0].piece.name == "D1" &&
+          context.board[4][1].piece.name == "D1" &&
+          context.board[3][0].piece.name == "D1") return true;
+      } catch (e) { }
+      return false;
     }
-    return true
-  },
-]
+  ]
