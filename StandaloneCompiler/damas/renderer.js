52ce78bd2f44e6aa24a84425b75e4b42fffe3d2c
bearkillerPT <gilteixeira@ua.pt>
2020-05-23 18:31:01 +0100

    merge

--- StandaloneCompiler/damas/renderer.js
+++ StandaloneCompiler/damas/renderer.js
@@ -43,17 +43,16 @@ const functions = {
   render: (context, table) => {
     const { board } = context
     functions.colorize_table(table, context)
-    if (context.focused.x == -1 || context.focused.y == -1) {
-      for (let x = 0; x < context.board.length; x++) {
-        for (let y = 0; y < context.board[x].length; y++) {
-          context.board[x][y].target = false
-          table[x][y].onclick = (event) => { }
-        }
-      }
-    }
     for (let x = 0; x < board.length; x++) {
       for (let y = 0; y < board[x].length; y++) {
-
+        if (context.focused.x != -1 && context.focused.y != -1) {
+          for (let x = 0; x < context.board.length; x++) {
+            for (let y = 0; y < context.board[x].length; y++) {
+              context.board[x][y].target = false
+              table[x][y].onclick = (event) => { }
+            }
+          }
+        }
         if (board[x][y].target) {
           table[x][y].style.backgroundColor = "rgba(100, 200, 100, 100)"
           table[x][y].onclick = (event) => {
@@ -67,6 +66,7 @@ const functions = {
             }
             board[x][y].piece = board[context.focused.x][context.focused.y].piece
             board[context.focused.x][context.focused.y].piece = null
+
             if (!board[x][y].piece.mirrored) {
               if (y === context.height - 1)
                 pieces[board[x][y].piece.name].on_end_reached(context, x, y)
