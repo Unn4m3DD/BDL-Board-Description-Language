c0b1a2f635228f2bf167bb41567cf1c91726e79a
Unn4m3DD <guerreiroapatacas@gmail.com>
2020-05-23 19:25:00 +0100

    mesclar bots

--- StandaloneCompiler/damas/abdl.js
+++ StandaloneCompiler/damas/abdl.js
@@ -59,17 +59,21 @@ const can_move = async (origin, destination) => {
 }
 export default {
   on_move: async (arg_context, arg_table, arg_render) => {
-      let v78 = new ABDLVar(context.current_player);
-      let v79 = new ABDLVar(1);
-      let v77 = v78.equal(v79);
-      if (v77.getValue() != 0) {
-         let v80 = new ABDLVar( await f0());
+      let v141 = new ABDLVar(context.current_player);
+      let v142 = new ABDLVar(0);
+      let v140 = v141.equal(v142);
+      if (v140.getValue() != 0) {
+         let v143 = new ABDLVar( await f4());
+      }
+      else {
+         let v144 = new ABDLVar( await f0());
       }
     },
   main: async (arg_context, arg_table, arg_render) => {
     context = arg_context;
     table = arg_table;
     render = arg_render;
+    let v139 = new ABDLVar( await f4());
   }
 }
 async function f0(){
@@ -107,7 +111,7 @@ async function f0(){
       if (v28.getValue() != 0) {
          let v31 = new ABDLVar([v22, v27]);
          let v32 = (await f1(v31, v5));
-         let v33 = (await f2(v32, v5)).less_than((await f2(v11, v5)));
+         let v33 = (await f2(v32, v5)).less_or_equal((await f2(v11, v5)));
          if (v33.getValue() != 0) {
             v11 = v32;
             let v34 = new ABDLVar([v22, v27]);
@@ -136,11 +140,11 @@ async function f1(v35, v36){
       if ((await can_move(v35, v49)).getValue() != 0) {
          let v53 = new ABDLVar([v45, v48]);
          let v52 = v35.add(v53);
-         let v51 = (await f2(v52, v36)).less_than((await f2(v42, v36)));
+         let v51 = (await f2(v52, v36)).less_or_equal((await f2(v42, v36)));
          if (v51.getValue() != 0) {
             let v56 = new ABDLVar([v45, v48]);
             let v55 = v35.add(v56);
-            let v54 = (await f2(v55, v36)).less_than((await f2(v35, v36)));
+            let v54 = (await f2(v55, v36)).less_or_equal((await f2(v35, v36)));
             if (v54.getValue() != 0) {
                let v58 = new ABDLVar([v45, v48]);
                let v57 = v35.add(v58);
@@ -177,4 +181,92 @@ async function f3(v72){
      let v75 = v76.sub(v72);
      return v75;
   }
+}
+async function f4(){
+  let v78 = new ABDLVar(0);
+  let v79 = new ABDLVar(0);
+  let v77 = new ABDLVar([v78, v79]);
+  let v80 = v77;
+  let v82 = new ABDLVar(0);
+  let v83 = new ABDLVar(0);
+  let v81 = new ABDLVar([v82, v83]);
+  let v84 = v81;
+  let v85 = new ABDLVar(0);
+  let v86 = v85;
+  let v87 = new ABDLVar(0);
+  let v89 = new ABDLVar(context.width);
+  let v90 = new ABDLVar(1);
+  let v88 = v89.sub(v90);
+  for(let v91 of range(v87,v88)) {
+    let v92 = new ABDLVar(0);
+    let v94 = new ABDLVar(context.height);
+    let v95 = new ABDLVar(1);
+    let v93 = v94.sub(v95);
+    for(let v96 of range(v92,v93)) {
+      let v98 = new ABDLVar([v91, v96]);
+      let v99 = new ABDLVar(0);
+      let v97 = (getOwner(v98)).equal(v99);
+      if (v97.getValue() != 0) {
+         let v100 = new ABDLVar([v91, v96]);
+         let v101 = (await f5(v100));
+         let v102 = (await f6(v101)).greater_than((await f6(v84)));
+         if (v102.getValue() != 0) {
+            let v103 = new ABDLVar([v91, v96]);
+            v80 = v103;
+            v84 = v101;
+         }
+      }
+    }
+  }
+  let v104 = v80.not_equal(v84);
+  if (v104.getValue() != 0) {
+     let v105 = v80.add(v84);
+     await move(v80, v105);
+  }
+}
+async function f5(v106){
+  let v108 = new ABDLVar(0);
+  let v109 = new ABDLVar(0);
+  let v107 = new ABDLVar([v108, v109]);
+  let v110 = v107;
+  let v111 = new ABDLVar(0);
+  let v112 = new ABDLVar(2);
+  for(let v113 of range(v111,v112)) {
+    let v114 = new ABDLVar(-2);
+    let v115 = new ABDLVar(0);
+    for(let v116 of range(v114,v115)) {
+      let v119 = new ABDLVar("->");
+      let v118 = v106.add(v119);
+      let v121 = new ABDLVar([v113, v116]);
+      let v120 = v106.add(v121);
+      let v117 = v118.add(v120);
+      console.log(v117.toString());
+      let v123 = new ABDLVar([v113, v116]);
+      let v122 = v106.add(v123);
+      console.log((await can_move(v106, v122)).toString());
+      let v125 = new ABDLVar([v113, v116]);
+      let v124 = v106.add(v125);
+      if ((await can_move(v106, v124)).getValue() != 0) {
+         let v127 = new ABDLVar([v113, v116]);
+         let v126 = (await f6(v127)).greater_than((await f6(v110)));
+         if (v126.getValue() != 0) {
+            let v128 = new ABDLVar([v113, v116]);
+            v110 = v128;
+         }
+      }
+    }
+  }
+  return v110;
+}
+async function f6(v129){
+  let v133 = new ABDLVar(0);
+  let v132 = new ABDLVar(v129.getValue()[v133.getValue()]);
+  let v134 = new ABDLVar(2);
+  let v131 = v132.pow(v134);
+  let v137 = new ABDLVar(1);
+  let v136 = new ABDLVar(v129.getValue()[v137.getValue()]);
+  let v138 = new ABDLVar(2);
+  let v135 = v136.pow(v138);
+  let v130 = v131.add(v135);
+  return v130;
 }
