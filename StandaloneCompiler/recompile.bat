390005d21bd702b01302c5f4410847144b32b709
Unn4m3DD <guerreiroapatacas@gmail.com>
2020-05-30 20:49:07 +0100

    correção de erros no relatorio

--- StandaloneCompiler/recompile.bat
+++ StandaloneCompiler/recompile.bat
@@ -1,10 +1,10 @@
 java -jar BDLCompiler.jar chess.bdl chess
 java -jar ABDLCompiler.jar matePastor.abdl chess
 
-java -jar BDLCompiler.jar sort.bdl Sort
-java -jar ABDLCompiler.jar bubbleSort.abdl Sort
+java -jar BDLCompiler.jar sort.bdl sort
+java -jar ABDLCompiler.jar bubbleSort.abdl sort
 
 java -jar BDLCompiler.jar pawngame.bdl pawnGame
 
-java -jar BDLCompiler.jar damas_chinesas_adaptadas.bdl damas
-java -jar ABDLCompiler.jar damasBots3vs3.abdl damas
+java -jar BDLCompiler.jar damasChinesasAdaptadas.bdl damas
+java -jar ABDLCompiler.jar damasBotVsBot.abdl damas
