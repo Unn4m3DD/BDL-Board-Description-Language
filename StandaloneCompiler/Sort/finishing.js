f8089e580d6be3e97e684f6bf9de68276f390dad
bearkillerPT <gilteixeira@ua.pt>
2020-06-03 16:10:28 +0100

    Finish rule para o bubble sort

--- StandaloneCompiler/Sort/finishing.js
+++ StandaloneCompiler/Sort/finishing.js
@@ -1,31 +1,17 @@
 import resolve_moves from "./resolve_moves.js"
 import pieces from "./pieces.js"
 export default [
-  (context) => {
-    for (let x = 0; x < context.width; x++) {
-      for (let y = 0; y < context.height; y++) {
-        if (context.board[x][y].piece
-          && context.board[x][y].piece.owner === context.current_player) {
-          let temp_board = JSON.parse(JSON.stringify(context.board))
-          resolve_moves(
-            pieces[temp_board[x][y].piece.name].moves,
-            x,
-            y,
-            {
-              board: temp_board,
-              height: context.height,
-              width: context.width,
-              current_player: context.current_player,
-              focused: context.focused
-            },
-            true)()
-          const targets = temp_board.map((e) => e.map((e2) => e2.target))
-          for (let i = 0; i < context.width; i++)
-            for (let j = 0; j < context.height; j++)
-              if (targets[i][j]) return false
+
+    (context) => {
+      let counter = 0;
+      for (let i = 0; i < context.width; i++) {
+          if (context.board[i][context.width - i - 1] != null &&
+          context.board[i][context.width - i - 1].piece != null &&
+            context.board[i][context.width - i - 1].piece.name != null &&
+            context.board[i][context.width - i - 1].piece.name == "p") counter++;
         }
-      }
+        console.log(counter);
+        if(counter == 9) return true; 
+    return false;
     }
-    return true
-  },
-]
+  ]
